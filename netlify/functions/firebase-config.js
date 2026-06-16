092e44fc6ad7de01072738f40c7615da4e0793e1
Gaurvi <gaurvi@Yevheniis-MacBook-Air.local>
2026-06-16 17:28:29 +0530

    add firebase config to .env and serverless function

--- netlify/functions/firebase-config.js
+++ netlify/functions/firebase-config.js
@@ -12,6 +12,7 @@ exports.handler = async () => {
       storageBucket: process.env.FIREBASE_STORAGE_BUCKET || '',
       messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID || '',
       appId: process.env.FIREBASE_APP_ID || '',
+      measurementId: process.env.FIREBASE_MEASUREMENT_ID || '',
     })
   };
 };
